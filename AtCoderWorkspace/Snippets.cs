a653224077e46c38aa608046e6a7c7ff458dac07
silatori <silatori1123@gmail.com>
2019-08-29 15:25:39 +0900

    add anippets
    Calculate the sum of numerical values for each digit(LINQ)

---
+++ AtCoderWorkspace/Snippets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtCoderWorkspace
+{
+    class Snippets
+    {
+        void LINQ()
+        {
+            //数値の各桁の和を計算
+            var digitSum = 10
+                .ToString()
+                .ToCharArray()
+                .Select(c => int.Parse(c.ToString()))
+                .Sum();
+        }
+    }
+}
