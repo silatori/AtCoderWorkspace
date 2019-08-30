c30ad17ec35da9391eb2ea7da776d2750f025111
silatori <silatori1123@gmail.com>
2019-08-30 10:14:42 +0900

    added PriorityQueue

--- AtCoderWorkspace/Program.cs
+++ AtCoderWorkspace/Program.cs
@@ -12,7 +12,7 @@ namespace AtCoderWorkspace
         static void Main(string[] args)
         {
             var cin = new Scanner();
-            var s = cin.next();       
+            var s = cin.next();
 
             Console.WriteLine();
         }
@@ -86,6 +86,58 @@ namespace AtCoderWorkspace
         }
     }
 
+    class PriorityQueue<T>
+    {
+        private readonly List<T> heap;
+        private readonly Comparison<T> compare;
+        private int size;
+        public PriorityQueue() : this(Comparer<T>.Default) { }
+        public PriorityQueue(IComparer<T> comparer) : this(16, comparer.Compare) { }
+        public PriorityQueue(Comparison<T> comparison) : this(16, comparison) { }
+        public PriorityQueue(int capacity, Comparison<T> comparison)
+        {
+            this.heap = new List<T>(capacity);
+            this.compare = comparison;
+        }
+        public void Enqueue(T item)
+        {
+            this.heap.Add(item);
+            var i = size++;
+            while (i > 0)
+            {
+                var p = (i - 1) >> 1;
+                if (compare(this.heap[p], item) <= 0)
+                    break;
+                this.heap[i] = heap[p];
+                i = p;
+            }
+            this.heap[i] = item;
+
+        }
+        public T Dequeue()
+        {
+            var ret = this.heap[0];
+            var x = this.heap[--size];
+            var i = 0;
+            while ((i << 1) + 1 < size)
+            {
+                var a = (i << 1) + 1;
+                var b = (i << 1) + 2;
+                if (b < size && compare(heap[b], heap[a]) < 0) a = b;
+                if (compare(heap[a], x) >= 0)
+                    break;
+                heap[i] = heap[a];
+                i = a;
+            }
+            heap[i] = x;
+            heap.RemoveAt(size);
+            return ret;
+        }
+        public T Peek() { return heap[0]; }
+        public int Count { get { return size; } }
+        public bool Any() { return size > 0; }
+    }
+
     class Scanner
     {
         string[] s;
@@ -156,8 +208,6 @@ namespace AtCoderWorkspace
         }
     }
 
-    #region ModInt
-
     public class ModInt
     {
         public static long Mod;
@@ -180,5 +230,4 @@ namespace AtCoderWorkspace
         }
         public static ModInt Inverse(ModInt v) { return Pow(v, Mod - 2); }
     }
-    #endregion
 }
