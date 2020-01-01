55540d1ce84e56fcde783892e595241d5c1cdded
silatori <silatori1123@gmail.com>
2020-01-01 18:42:36 +0900

    add IsPrime

--- AtCoderWorkspace/Library.cs
+++ AtCoderWorkspace/Library.cs
@@ -97,6 +97,24 @@ namespace AtCoderWorkspace
 
             return isPrime;
         }
+        public static bool IsPrime(int num)
+        {
+            if (num < 2) return false;
+            else if (num == 2) return true;
+            else if (num % 2 == 0) return false;
+
+            double sqrtNum = Math.Sqrt(num);
+            for (int i = 3; i <= sqrtNum; i += 2)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
     }
 
     public static class Combination
