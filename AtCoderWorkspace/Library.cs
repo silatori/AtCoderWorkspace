c64a558aaefb9aedeaf3c7509ee1b0f2dc2ee8ec
silatori <silatori1123@gmail.com>
2020-01-14 20:48:54 +0900

    [add] grid scan

--- AtCoderWorkspace/Library.cs
+++ AtCoderWorkspace/Library.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Text;
 using Number = System.Int64;
 
-namespace AtCoderWorkspace
+namespace AtCoderLibrary
 {
     class Library
     {
+        void main()
+        {
+            var xx = new int[] { 1, 0, -1, 0 }; // 右 下 左 上
+            var yy = new int[] { 0, -1, 0, 1 };
+
+            // グリッド取得
+            var cin = new Scanner();
+            var h = cin.nextInt();
+            var w = cin.nextInt();
+            var f = new int[h, w];
+            for (int i = 0; i < h; i++)
+            {
+                var row = cin.next().ToCharArray();
+                for (int j = 0; j < w; j++)
+                {
+                    if (row[j] == '.')
+                    {
+                        f[i, j] = 0;
+                    }
+                    else
+                    {
+                        f[i, j] = 1;
+                    }
+                }
+            }
+        }        
     }
 
     class Modular
@@ -497,6 +523,96 @@ namespace AtCoderWorkspace
                     ret[i] = this[i, i];
                 return ret;
             }
+        }
+    }
+
+    class Scanner
+    {
+        string[] s;
+        int i;
+
+        char[] cs = new char[] { ' ' };
+
+        public Scanner()
+        {
+            s = new string[0];
+            i = 0;
+        }
+
+        public string next()
+        {
+            if (i < s.Length) return s[i++];
+            string st = Console.ReadLine();
+            while (st == "") st = Console.ReadLine();
+            s = st.Split(cs, StringSplitOptions.RemoveEmptyEntries);
+            if (s.Length == 0) return next();
+            i = 0;
+            return s[i++];
+        }
+
+        public int nextInt()
+        {
+            return int.Parse(next());
+        }
+        public int[] ArrayInt(int N, int add = 0)
+        {
+            int[] Array = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = nextInt() + add;
+            }
+            return Array;
+        }
+
+        public int[] DPArrayInt(int N, int ex)
+        {
+            int[] Array = new int[N + ex];
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = nextInt();
+            }
+            return Array;
+        }
+
+        public long nextLong()
+        {
+            return long.Parse(next());
+        }
+
+        public long[] ArrayLong(int N, long add = 0)
+        {
+            long[] Array = new long[N];
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = nextLong() + add;
+            }
+            return Array;
+        }
+
+        public long[] DPArrayLong(int N, int ex)
+        {
+            long[] Array = new long[N + ex];
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = nextLong();
+            }
+            return Array;
+        }
+
+        public double nextDouble()
+        {
+            return double.Parse(next());
+        }
+
+
+        public double[] ArrayDouble(int N, double add = 0)
+        {
+            double[] Array = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                Array[i] = nextDouble() + add;
+            }
+            return Array;
         }
     }
 }
